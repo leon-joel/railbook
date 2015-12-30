1b0db19d19df6880d97948e483f799df5352b0bb
L.Joel <leopard10000@gmail.com>
2015-12-31 08:48:46 +0900

    end of 3.6

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151229042303) do
+ActiveRecord::Schema.define(version: 20151229114822) do
 
   create_table "books", force: true do |t|
     t.string   "isbn"
